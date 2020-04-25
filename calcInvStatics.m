c5abccaee10d873626340cc3f303cb5cb3588751
unknown <mkshah909@gmail.com>
2020-04-25 19:35:32 -0400

    Initial Commit

---
+++ calcInvStatics.m
@@ -0,0 +1,19 @@
+function [Fsensor] = calcInvStatics(Ftool, gSensor, gToolCG)
+% Fsensor: 6x1 Reaction force vector measured by sensor (reaction force,
+%          NOT force ON sensor
+% gSensor: 4x4 current position and orientation of sensor face
+% gToolCG: 4x4 current position and orientation of tool CG
+% Ftool: 6x1 Total force vector at tool CG, Ftool = Fapp + mg
+
+R = gToolCG(1:3,end) - gSensor(1:3,end);
+
+Rhat = [    0  -R(3)  R(2);
+         R(3)     0  -R(1);
+        -R(2)   R(1)     0]; %[mm]
+    
+A = [eye(3) zeros(3,3);
+     Rhat   eye(3)];
+ 
+Fsensor = A*Ftool;
+
+end
