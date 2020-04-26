0ab28d536bc6f9c545a91080c1b9f1c162bf604d
unknown <mkshah909@gmail.com>
2020-04-25 21:34:06 -0400

    Initial Commit

---
+++ gravityComp.m
@@ -0,0 +1,13 @@
+function FEstApp = gravityComp(gCompBool, mTool, g, FEstA, gSensor)
+% gCompBool: gravity compensation turned on or off
+% Ftool: 6x1 force vector
+% gSensor: 4x4 position and orientation of sensor
+% FEstApp: 6x1 estimate of applied force on tool after subtracting gravity
+
+FEstApp = Ftool;
+if gCompBool
+    RotMat = inv(gSensor(1:3,1:3));
+    gRot = RotMat*g;
+    FEstA(1:3) = FEstA(1:3) - mTool.*gRot;
+end
+end
