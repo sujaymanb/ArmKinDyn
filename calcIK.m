088b12dd56eb11e224f8e26c9191aaa90c2377b1
sujaymanb <sujaymanb@gmail.com>
2020-04-28 17:17:59 -0400

    calc IK wrt toolSurface

---
+++ calcIK.m
@@ -0,0 +1,41 @@
+function theta = calcIK(pose,thetaInit,q,w,gSensor0,gToolSurface0,gToolCG0)
+    pd = pose(1:3);
+    qd = quaternion([pose(7),pose(4:6)']);
+    
+    error = 1;
+    iter = 0;
+    
+    kp = 1;
+    ko = 3;
+    step = 0.002;
+    
+    thetaCur = thetaInit;
+    
+    thetas = nan(5000, 7);
+    thetas(1,:) = thetaCur';
+    
+    twists = calcTwists(q,w);
+    
+    while((error > 1e-3) && (iter < 5000))
+       iter = iter + 1;
+       [gSensor, gToolSurface, gToolCG, jointPos] = calcFK(thetaCur,q,w,gSensor0,gToolSurface0,gToolCG0);
+      
+       g = gToolSurface;
+       pc = g(1:3,4);
+       qc = quaternion(g(1:3,1:3),'rotmat','point');
+       
+       %fprintf('position: ');
+       %fprintf('%g ',pc');
+       %fprintf('%g ',qc.compact);
+       %fprintf('\n');
+       
+       [v,error] = calcV(pd, qd, pc, qc, kp, ko);
+       J = jacobian(twists, thetaCur);
+       thetaDot = pinv(J)*v;
+       thetaCur = thetaCur + thetaDot * step;
+       
+       thetas(iter+1,:) = thetaCur';
+    end
+    %fprintf('iterations: %g',iter);
+    theta = thetaCur;
+end
