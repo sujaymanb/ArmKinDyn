157bec81a22fac89b985bcb01961e698d3f6362d
unknown <mkshah909@gmail.com>
2020-04-28 20:56:21 -0400

    added comments and formatting

--- calcIKSingleStep.m
+++ calcIKSingleStep.m
@@ -4,23 +4,26 @@ function theta = calcIKSingleStep(FEstApp,thetaInit,q,w,currPose)
 pc = currPose(1:3,4);
 qc = currPose(1:3,1:3);
 
+% Direction and magnitude of force
 force = FEstApp(1:3);
 kp = norm(force).*1;
 if norm(force) ~= 0
     force = force/norm(force);
 end
 
+% Direction and magnitude of torque
 torque = FEstApp(4:6);
 kq = norm(torque)*1;
 if norm(torque) ~= 0
     torque = torque/norm(torque);
 end
 
+% Translational velocity using force
 velT = kp*force;
 
+% Rotational velocity using torque
 xsquat = quaternion(eul2quat([0 0 0]));
-xd1quat = quaternion(eul2quat([0 0 0]));
-
+xd1quat = quaternion(eul2quat(torque));
 xomega1 = xd1quat./xsquat;
 [wd1, id1, jd1, kd1] = parts(xomega1);
 id1 = sign(wd1)*id1;
@@ -28,10 +31,12 @@ jd1 = sign(wd1)*jd1;
 kd1 = sign(wd1)*kd1;
 V = [velT; id1; jd1; kd1];
 
+% Jacobian of arm
 twists = calcTwists(q,w);
-step = 0.01;
-
 J = jacobian(twists, thetaInit);
+
+% Step in direction of end effector velocity
+step = 0.01;
 thetaDot = pinv(J)*V;
 theta = thetaInit + thetaDot*step;
 
