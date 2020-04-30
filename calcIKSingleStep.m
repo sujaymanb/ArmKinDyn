c15db82482385a125c182c40ab8c09b8a08530e0
unknown <mkshah909@gmail.com>
2020-04-30 14:42:26 -0400

    added gains as function inputs

--- calcIKSingleStep.m
+++ calcIKSingleStep.m
@@ -1,16 +1,16 @@
-function theta = calcIKSingleStep(FEstApp,thetaInit,q,w,currPose)
+function theta = calcIKSingleStep(FEstApp,thetaInit,q,w,currPose,kp,kq)
 % currPose: 4x4 gToolCG
 
 % Direction and magnitude of force
 force = FEstApp(1:3);
-kp = norm(force).*1;
+kp = norm(force).*kp;
 if norm(force) ~= 0
     force = force/norm(force);
 end
 
 % Direction and magnitude of torque
 torque = FEstApp(4:6);
-kq = norm(torque)*1;
+kq = norm(torque)*kq;
 if norm(torque) ~= 0
     torque = torque/norm(torque);
 end
