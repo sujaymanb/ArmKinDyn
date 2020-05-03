b5546a67c99a78a2e11fd352636f69f4ce5ce559
sujaymanb <sujaymanb@gmail.com>
2020-05-02 22:31:16 -0400

    axisGuidance added, plotting wrt tool surface

--- runModel.m
+++ runModel.m
@@ -25,7 +25,7 @@ mode = 6;
 %% !!!DO NOT EDIT BELOW!!!
 run('loadSysParams.m')
 kp = 0.2;
-kq = 0.2;
+kq = 0.5;
 T = 80;
 maxF = 15; %[N]
 maxTau = 10; %[Nm]
@@ -42,7 +42,7 @@ end
 
 axisGuidanceBool = false;
 if mode==8
-    axisGuidanceBool = True;
+    axisGuidanceBool = true;
 end
 
 %% Initialize system
@@ -61,26 +61,32 @@ theta = theta0; % track joint angles independently for plotting
 % % --------------------
 
 % Initialize animation figure
-figure()
+figure('Renderer', 'painters', 'Position', [10 10 900 600])
 hold off
 toolPos = [];
-axis = [1, 0, 0;
-        0, 1, 0;
-        0, 0, 1];
+axis = [0.1, 0, 0;
+        0, 0.1, 0;
+        0, 0, 0.1;
+        1, 1, 1];
 for t = 1:size(FtoolSim,2)
     % Simulator:
     % Compute FK and simulated Fsensor
-    toolPos = [toolPos gSensor(1:3,end)];
+    toolPos = [toolPos gToolSurface(1:3,end)];
     Fsensor = calcInvStatics(FtoolSim(:,t), gSensor, gToolCG);
     
     % Controller:
     % Compute Ftool from Fsensor and FK
     Ftool = calcStatics(Fsensor, gSensor, gToolCG);
     % Plot arm links and tool trajectory
-    plot3(toolPos(1,:), toolPos(2,:), toolPos(3,:), 'r-', 'LineWidth', 1.5)
+    plot3(toolPos(1,:), toolPos(2,:), toolPos(3,:), 'k-', 'LineWidth', 1.5)
     hold on
     plot3(jointPos([1, 2, 4, 6, 7],1), jointPos([1, 2, 4, 6, 7],2), jointPos([1, 2, 4, 6, 7],3), 'bo-', 'LineWidth', 1.5)
     plot3([jointPos(7,1) toolPos(1,end)], [jointPos(7,2) toolPos(2,end)], [jointPos(7,3) toolPos(3,end)], 'g-', 'LineWidth', 5)
+    % plot axis
+    rotAxis = gToolSurface * axis;
+    plot3([gToolSurface(1,end),rotAxis(1,1)],[gToolSurface(2,end),rotAxis(2,1)],[gToolSurface(3,end),rotAxis(3,1)], 'r-', 'LineWidth', 1.5)
+    plot3([gToolSurface(1,end),rotAxis(1,2)],[gToolSurface(2,end),rotAxis(2,2)],[gToolSurface(3,end),rotAxis(3,2)], 'g-', 'LineWidth', 1.5)
+    plot3([gToolSurface(1,end),rotAxis(1,3)],[gToolSurface(2,end),rotAxis(2,3)],[gToolSurface(3,end),rotAxis(3,3)], 'b-', 'LineWidth', 1.5)
     hold off
     legend('Tool Path', 'Arm Links', 'Tool')
     grid on
@@ -96,10 +102,10 @@ for t = 1:size(FtoolSim,2)
     FEstApp = gravityComp(gravCompBool, mTool, g, Ftool);
     if axisGuidanceBool
         % cancel non-tool axis components of force with new function
-        % FEstApp = 
+        FEstApp = axisGuidance(FEstApp, gToolSurface);
     end
-    disp(' Fapplied    FtoolSim    Fsensor    Ftool    FEstApp')
-    disp([Fapplied(:,t), FtoolSim(:,t), Fsensor, Ftool, FEstApp])
+    %disp(' Fapplied    FtoolSim    Fsensor    Ftool    FEstApp')
+    %disp([Fapplied(:,t), FtoolSim(:,t), Fsensor, Ftool, FEstApp])
     % Obtain desired pose from FEstApp, perform IK, and obtain newTheta
     theta2 = calcIKSingleStep(FEstApp,theta,q,w,gToolCG,kp,kq);
     [gSensor, gToolSurface, gToolCG, jointPos] = calcFK(theta2,q,w,gSensor0,gToolSurface0,gToolCG0);
